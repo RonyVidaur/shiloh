dce21a5f9bcfb7fd7863a3c014c52c7b57a16e3c
Rony Vidaur <rony.vidaur@gmail.com>
2017-05-06 21:39:32 -0600

    basic components and template

---
+++ src/pages/Start.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+  <div class="content">
+    <div class="container">
+      <div class="search-container">
+        <input class="search-input"
+        @keyup.enter="requestData"
+        placeholder="Search in the SW world"
+        type="search" name="search"
+        v-model="package"
+        >
+        <button class="search-button" @click="requestData">Find</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
